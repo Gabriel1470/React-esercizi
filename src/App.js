0150b6ad5ed6694c6c3ed9dc763f1760f2a0e70b
Gabriel Bogdan <99339866+Gabriel1470@users.noreply.github.com>
2022-05-25 09:56:06 +0200

    esercizio 05

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-
+/* esercizio 01 */
 let Hello = <><h1>Hello World!</h1></>
-
+/* esercizio 02 */
 function Name(param){
    param = "Jhon"
   return(
     <h1>Hello {param}</h1>
   )
 }
+/* esercizio 05 */
+function Sum(a,b){
+  a=4; b=5;
+  
+   return (
+     <h2>{a+b}</h2>
+   )
+  
+}
+
 
 function App() {
   return (
@@ -29,6 +39,7 @@ function App() {
       </header>
       {Hello}
       <Name/>
+      <Sum/>
     </div>
   );
 }
