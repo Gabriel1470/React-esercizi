11fe0963022b5d74ab9ccd2c3f45e5201184037c
Gabriel Bogdan <99339866+Gabriel1470@users.noreply.github.com>
2022-05-25 08:46:07 +0200

    Esercizio jsx 01

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+let Hello = <><h1>Hello World!</h1></>
+
 function App() {
   return (
     <div className="App">
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      {Hello}
     </div>
   );
 }
