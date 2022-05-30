76fb5c057899e7b6dc6e3851840a86dacbabf38f
Gabriel Bogdan <99339866+Gabriel1470@users.noreply.github.com>
2022-05-30 13:03:36 +0200

    props 05

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Age } from "./Age";
 
  export class Welcome extends React.Component{
   
@@ -9,7 +10,7 @@ import React from "react";
 
         {!! this.props.name  && <h1>Welcome, {this.props.name}!</h1> }
        {!! this.props.age && <p>Your age is {this.props.age}</p>}
-     
+     <Age age={34}/>
       </div>
     )
   }
