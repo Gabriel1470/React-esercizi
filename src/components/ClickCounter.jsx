fdd3b36d699be113e7e465d1ccb8e18467a572c4
Gabriel Bogdan <99339866+Gabriel1470@users.noreply.github.com>
2022-06-13 19:53:26 +0200

    events 03

--- src/components/ClickCounter.jsx
+++ src/components/ClickCounter.jsx
@@ -2,19 +2,21 @@ import React from "react";
 
 export class ClickCounter extends React.Component {
   state = {
-    count: this.props.initValue ?? 0,
+    btnName: '',
   }
 
-  incrementState=()=>{
-this.setState({count: this.state.count + 1})
+  incrementState=(event)=>{
+this.setState({btnName: event.target.innerText})
   }
 
   render() {
     return (
       <div>
-        <h1>Count: {this.state.count}</h1>
+        <h1>Last button:{this.state.btnName}</h1>
 
-<button onClick={this.incrementState}>Click meeeeeeeeeeeeee!!!!!!!!!!!</button>
+<button name="btn-1" onClick={this.incrementState}>Click meeeeeeeeeeeeee!!!!!!!!!!!</button>
+<button name="btn-2" onClick={this.incrementState}>Nooooooooo Click meeee!!!!</button>
+<button name="btn-ita" onClick={this.incrementState}>perche non io scusa</button>
 
       </div>
     )
