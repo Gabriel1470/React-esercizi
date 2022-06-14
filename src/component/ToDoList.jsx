509c5d49e263905aa4280c63f3ce6584705f5212
Gabriel Bogdan <99339866+Gabriel1470@users.noreply.github.com>
2022-06-14 13:21:19 +0200

    list 03

---
+++ src/component/ToDoList.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { createRef } from "react";
+
+export class ToDoList extends React.Component{
+inpt = createRef()
+state = {
+  item:['maserati','audi','bmw','fiat',],
+  addItem:''
+}
+
+eventHandler = (event) => {
+
+  this.setState((state)=>{
+    console.log()
+    return {addItem: state.addItem = event.target.value}
+  })
+}
+
+saveHandler=()=>{
+  this.setState((state)=>{
+    console.log()
+    return{item:state.item.concat(this.state.addItem)}
+  })
+}
+
+render(){
+  return(
+    <div>
+      <ul>
+        {this.state.item.map((item,index)=>(<li key={index}>{item}</li>))}
+      </ul>
+      <input type="text" value={this.state.addItem} onChange={this.eventHandler}/>
+      <button onClick={this.saveHandler}>Change</button>
+    </div>
+  )
+}
+
+}
