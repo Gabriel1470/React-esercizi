abf61d4346cc5fb999f8463a282546c8e46c31a2
Gabriel Bogdan <99339866+Gabriel1470@users.noreply.github.com>
2022-06-14 12:47:41 +0200

    forms 06

--- src/component/UncontrolledLogin.jsx
+++ src/component/UncontrolledLogin.jsx
@@ -1,51 +1,38 @@
-import react, { createRef } from 'react';
+import React from "react";
+import { createRef } from "react";
 
-export class UncontrolledLogin extends react.Component {
-    _formRef = createRef()
 
-    inputHandler = (event) => {
-        event.preventDefault()
+export class UncontrolledLogin extends React.Component{
+_autoFocus = createRef()
+_formRef = createRef()
 
-        const username = event.target.elements.username.value
-        const password = event.target.elements.password.value
-        const remember = event.target.elements.remember.checked
+imputHandler=(event)=>{
+  event.preventDefault()
+ 
+  const username = event.target.elements.username.value
+  const password = event.target.elements.password.value
+  const remember = event.target.elements.remember.checked
 
-        console.log(
-            {
-                username,
-                password,
-                remember
-            }
-        )
+  console.log({
+    username,password,remember
+  })
 
-    }
+}
+ componentDidMount=()=>{
+    this._autoFocus.current.focus()
+  }
 
-    handlerFormPrefill = () => {
-        this._formRef.current.elements.username.value = "Jhones"
-        this._formRef.current.elements.password.value = "passwordsicura123"
-        this._formRef.current.elements.remember.checked = true;
-
-    }
-
-    render() {
-
-        return (
-            <div>
-                <form ref={this._formRef} onSubmit={this.inputHandler}>
-                    <div>
-                        <h3>Nome:</h3>
-                        <input type="text" name="username" /></div>
-                    <div>
-                        <h3>Password:</h3>
-                        <input name="password" type="password" />
-                        <input name="remember" type="checkbox" />
-                        <button type="submit">Login</button>
-                        <button type='reset' >Reset</button>
-                        <button type='button' onClick={this.handlerFormPrefill}>Prefill</button>
-                    </div>
-                </form>
-            </div>
-        )
-    }
-
+  render(){
+    return(
+   <form ref={this._formRef} onSubmit={this.imputHandler}>
+<h2>Name:</h2>
+<input type="text" name="username"  ref={this._autoFocus} />
+<h2>Password:</h2>
+<input type="password" name="password" />
+<input type="checkbox" name="remember" />
+<button type='submit'>Login</button>
+<button >Reset</button>
+   </form>
+    )
+  }
 }
