2c3ec529af45ce08e032d36f55cf21da2c72b3db
Gabriel Bogdan <99339866+Gabriel1470@users.noreply.github.com>
2022-06-17 18:30:58 +0200

    useEffect 03

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
 
-import { ClickCounter } from "./components/ClickCounter";
-import { Login } from "./components/Login";
-import {useState} from 'react'
-
+import {GithubUser} from './components/GithubUser'
 
 function App() {
 
-const [toggle,setToggle] = useState(true)
-
-
-
-
-  function onCounterChange(clock){
-    console.log(`The state now is ${clock}`)
-  }
-
-  function btnHandler(){
-    setToggle(y=>!y)
-  }
-
-
-  return (
-    <div>
-      <Login/>
-   {toggle && <ClickCounter onCounterChange={onCounterChange}/>}
-   <button onClick={btnHandler}>Reset</button>
-    </div>
-  );
+return(
+  <div>
+<GithubUser username={'Gabriel1470'}/>
+  </div>
+)
 }
 
 export default App;
---
+++ src/components/GithubUser.jsx
@@ -0,0 +1,36 @@
+import { useEffect , useState } from "react";
+
+export function GithubUser({username}){
+
+const [dati,setDati] = useState('')
+const [err,setErr] = useState(false)
+
+async function userFetch() {
+  try {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    if(response.status === 200){
+      const info = await response.json()
+      setDati(info)
+    }
+    else{
+throw new Error('User inesistente/impossibile comunicare con il server')
+    }
+  }catch(err){
+    return setErr(err)
+  }
+}
+
+useEffect(()=>{
+  userFetch()
+},[])
+
+
+return(
+<div>
+<h1>{dati.login}</h1>
+<img src={dati.avatar_url} alt="/"></img>
+{err && <h1>{err.message}</h1>}
+</div>
+)
+
+}
