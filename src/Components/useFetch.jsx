dd56aa1468b9a2a8d88c023427269433a23e3a0a
Gabriel Bogdan <99339866+Gabriel1470@users.noreply.github.com>
2022-06-22 13:33:29 +0200

    Custom Hooks 04

--- src/Components/useFetch.jsx
+++ src/Components/useFetch.jsx
@@ -1,12 +1,13 @@
 import { useState,useEffect } from "react"
 
 export default function useFetch(username){
-
+  const [loading,setLoading]=useState(false)
     const [data,setData]=useState(username)
     const [error,setError]=useState(false)
 
     async function userProvider (){ 
     try {
+      setLoading(true)
         const response = await fetch(`https://api.github.com/users/${username}`);
         if(response.status === 200){
       const pack = await response.json()
@@ -16,6 +17,7 @@ export default function useFetch(username){
       else{
           throw new Error('Impossibile comunicare con il server.');
         }
+        setLoading(false)
     }catch(errore){
         return setError(errore);
     }
@@ -26,6 +28,6 @@ userProvider()
 
    },[])
 
-   return{data,error}
+   return{data,error,loading}
 
 }
