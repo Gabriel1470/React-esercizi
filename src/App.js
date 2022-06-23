bf6e65056c5b485a3e78cba520efe442ab2d6832
Gabriel Bogdan <99339866+Gabriel1470@users.noreply.github.com>
2022-06-23 15:04:45 +0200

    React Root 02
    pleh

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Counter from './Components/Counter';
 import Welcome from './Components/Welcome'
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Welcome name='NullNanNBan' />}> </Route>
+        <Route path='/counter' element={<Counter />}></Route>
       </Routes>
     </div>
   );
