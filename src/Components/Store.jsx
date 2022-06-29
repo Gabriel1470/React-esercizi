44654ce905eb821a19a06c5ad10e6813a04af764
Gabriel Bogdan <99339866+Gabriel1470@users.noreply.github.com>
2022-06-29 13:54:52 +0200

    redux 04

--- src/Components/Store.jsx
+++ src/Components/Store.jsx
@@ -6,7 +6,7 @@ const rootReducer=combineReducers({
   counter: CounterReducer,
   todo: todoReducer
 })
-
+/* fatto redux 04 senza saperlo */
 export const store = createStore(rootReducer)
 
 store.subscribe(()=>{console.log(store.getState())})
